61fb0f5aff4721c06fc80181051afc779f4e07ef
erikk <ekeszler77@gmail.com>
2023-09-25 15:45:17 +0300

    merge problema 1 pana la addToBlacklist

--- src/main/java/Probleme_incomode/pb1/Main.java
+++ src/main/java/Probleme_incomode/pb1/Main.java
@@ -7,15 +7,33 @@ public class Main {
 
     public static void main(String[] args) {
         PhoneBook phoneBook = new PhoneBook();
+
         List<String> allContacts = new ArrayList<>();
         allContacts.add("23");
         allContacts.add("34");
         allContacts.add("56");
+
         List<String> blackList = new ArrayList<>();
         blackList.add("23");
-        blackList.add("33");
         blackList.add("34");
-        System.out.println(PhoneBook.filterContacts(allContacts, blackList));
+        blackList.add("56");
+
+        phoneBook.setContacts(allContacts);
+        phoneBook.setBlackList(blackList);
+
+        PhoneBook phoneBook1 = new PhoneBook(allContacts, blackList);
+
+        phoneBook.getContacts().add("23");
+        phoneBook.getContacts().add("34");
+        phoneBook.getContacts().add("56");
+        // List<String> blackList = new ArrayList<>();
+        phoneBook.getBlackList().add("23");
+        phoneBook.getBlackList().add("33");
+        phoneBook.getBlackList().add("34");
+        //System.out.println(PhoneBook.filterContacts(allContacts, blackList));
+        phoneBook.addToBlackList("56");
+        System.out.println(phoneBook.getContacts());
+        System.out.println(phoneBook.getBlackList() );
 
     }
 }
--- src/main/java/Probleme_incomode/pb1/PhoneBook.java
+++ src/main/java/Probleme_incomode/pb1/PhoneBook.java
@@ -1,20 +1,53 @@
 package Probleme_incomode.pb1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PhoneBook {
 
-    List<String> contacts;
-    List<String> blackList;
+    private List<String> contacts;
+    private List<String> blackList;
 
+    public PhoneBook(List<String> contacts, List<String> blackList) {
+        this.contacts = contacts;
+        this.blackList = blackList;
+    }
+
+    public PhoneBook(){
+        this.contacts = new ArrayList<>();
+        this.blackList = new ArrayList<>();
+    }
+
+    public List<String> getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(List<String> contacts) {
+        this.contacts = contacts;
+    }
+
+    public List<String> getBlackList() {
+        return blackList;
+    }
+
+    public void setBlackList(List<String> blackList) {
+        this.blackList = blackList;
+    }
 
     public static List<String> filterContacts(List<String> allContacts, List<String> blackList){
-        for(String contact: allContacts){
-            if(blackList.contains(contact)){
-                allContacts.remove(contact);
+        for(String blackListContact: blackList){
+            if(allContacts.contains(blackListContact)){
+                allContacts.remove(blackListContact);
             }
         }
         return allContacts;
+    }
+
+    public void addToBlackList(String phoneNumber){
+        blackList.add(phoneNumber);
+        contacts.remove(phoneNumber);
+        System.out.println(blackList);
+        System.out.println(contacts);
     }
 
 }
